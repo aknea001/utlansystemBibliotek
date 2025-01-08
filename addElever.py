79bb72072665051f53e8e00708440d4a02f8b2be
Aksel <aknea001@osloskolen.no>
2025-01-08 12:46:50 +0100

    lagde et kjapt script som legger til random elever

---
+++ addElever.py
@@ -0,0 +1,58 @@
+import mysql.connector
+from dotenv import load_dotenv
+from os import getenv
+import requests
+from random import randint, choice
+
+load_dotenv()
+
+sqlConfig = {
+    "host": getenv("SQLHOST"),
+    "user": getenv("SQLUSER"),
+    "password": getenv("SQLPASSWD"),
+    "database": getenv("SQLDATABASE")
+}
+
+def main(fornavn, etternavn, programfag):
+    try:
+        db = mysql.connector.connect(**sqlConfig)
+        cursor = db.cursor()
+
+        query = "INSERT INTO elever (fornavn, etternavn, programfag) \
+                VALUES\
+                (%s, %s, %s)"
+        
+        cursor.execute(query, (fornavn, etternavn, programfag))
+        db.commit()
+    except mysql.connector.Error as e:
+        db = None
+        print(f"Oopsie: {e}")
+    finally:
+        if db != None and db.is_connected():
+            cursor.close()
+            db.close()
+
+if __name__ == "__main__":
+    for i in range(20):
+        url = "https://randomuser.me/api/?nat=no,dk,nl,us"
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            fullName = response.json()["results"][0]["name"]
+            first = fullName["first"]
+            last = fullName["last"]
+
+        linjer1 = ["ST", "MK", "IM"]
+        linjer2 = ["ST", "MK", "MP", "IT"]
+        trinn = randint(1, 2)
+        if trinn == 1:
+            linje = choice(linjer1)
+        else:
+            linje = choice(linjer2)
+
+        programfag = str(trinn) + str(linje)
+
+        print(f"First: {first} \nLast: {last} \nProgramfag: {programfag}")
+
+        main(first, last, programfag)
