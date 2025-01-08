a61d1df35465c65e51c21125250daf810a852e9a
aknea001 <aknea001@osloskolen.no>
2025-01-08 19:39:10 +0100

    la til utlån info til api elev endpoint

--- api/main.py
+++ api/main.py
@@ -20,27 +20,50 @@ def elev(elevID):
         db = mysql.connector.connect(**sqlConfig)
         cursor = db.cursor()
 
-        query = "SELECT * FROM elever WHERE id = %s"
+        query = "SELECT\
+                    e.*, \
+                    GROUP_CONCAT(b.id SEPARATOR ',') AS bokIDer, \
+                    GROUP_CONCAT(b.navn SEPARATOR ',') AS bokNavn, \
+                    GROUP_CONCAT(IFNULL(b.forfatter, 'Ukjent Forfatter') SEPARATOR ',') AS bokForfattere, \
+                    GROUP_CONCAT(IFNULL(b.sjanger, 'Ukjent Sjanger') SEPARATOR ',') AS bokSjangere \
+                FROM elever e \
+                LEFT JOIN utlan u ON e.id = u.elevID \
+                LEFT JOIN boker b ON u.bokID = b.id \
+                WHERE e.id = %s \
+                GROUP BY e.id;"
 
         cursor.execute(query, (elevID, ))
-
         data = cursor.fetchone()
-        dataDic = {
-            "id": data[0], 
-            "first": data[1], 
-            "last": data[2], 
-            "programfag": data[3], 
-            "registrert": data[4]
-        }
 
-        return jsonify(dataDic)
+        if data:
+            dataDic = {
+                "userInfo": {
+                    "id": data[0], 
+                    "name": {
+                        "first": data[1], 
+                        "last": data[2]
+                    }, 
+                    "programfag": data[3], 
+                    "registrert": data[4]
+                },
+                "utlanInfo": {
+                    "bokIDer": data[5].split(","),
+                    "bokNavn": data[6].split(","),
+                    "bokForfattere": data[7].split(","),
+                    "bokSjangere": data[8].split(",")
+                }
+            }
+        else:
+            return jsonify({"error": "No data found"}), 404
     except mysql.connector.Error as e:
         db = None
-        print(f"oopsie: {e}")
+        return f"oopsie: {e}"
     finally:
         if db != None and db.is_connected():
             cursor.close()
             db.close()
+
+    return jsonify(dataDic)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
