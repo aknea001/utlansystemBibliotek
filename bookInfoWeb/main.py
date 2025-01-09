1a7563388c1c4cc5f853a0719c960ad00312d72c
Aksel <aknea001@osloskolen.no>
2025-01-09 09:20:50 +0100

    kan leie til elever fra bokInfo nettsiden

--- bookInfoWeb/main.py
+++ bookInfoWeb/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, url_for
 from dotenv import load_dotenv
 from os import getenv
 import requests
@@ -8,32 +8,41 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = getenv("FLASKPASSWD")
 
-@app.route("/<bokID>")
+@app.route("/<bokID>", methods=["GET", "POST"])
 def index(bokID):
     url = f"http://localhost:8000/bok/{bokID}"
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        responseJson = response.json()
-        bokInfo = responseJson["bokInfo"]
-        elevInfo = responseJson["elevInfo"]
+    if request.method == "GET":
+        response = requests.get(url)
 
-        navn = bokInfo["navn"]
-        forfatter = bokInfo["forfatter"]
-        sjanger = bokInfo["sjanger"]
-        hylle = bokInfo["hylle"]
+        if response.status_code == 200:
+            responseJson = response.json()
+            bokInfo = responseJson["bokInfo"]
+            elevInfo = responseJson["elevInfo"]
 
-        elevNavn = f"{elevInfo['navn']['first']} {elevInfo['navn']['last']}"
-        programfag = elevInfo["programfag"]
+            navn = bokInfo["navn"]
+            forfatter = bokInfo["forfatter"]
+            sjanger = bokInfo["sjanger"]
+            hylle = bokInfo["hylle"]
 
-        if responseJson["leid"]:
-            session["leid"] = True
+            elevNavn = f"{elevInfo['navn']['first']} {elevInfo['navn']['last']}"
+            programfag = elevInfo["programfag"]
+
+            if responseJson["leid"]:
+                session["leid"] = True
+            else:
+                session.clear()
+
+            return render_template("index.html", navn=navn, forfatter=forfatter, sjanger=sjanger, hylle=hylle, elevNavn=elevNavn, elevProg=programfag)
         else:
-            session.clear()
-
-        return render_template("index.html", navn=navn, forfatter=forfatter, sjanger=sjanger, hylle=hylle, elevNavn=elevNavn, elevProg=programfag)
+            return "nope"
     else:
-        return "nope"
+        form = request.form
+        elevID = form["elevID"]
+        dager = form["dager"]
+
+        response = requests.post(url, json={"elevID": elevID, "dager": dager})
+        return redirect(url_for("index", bokID=bokID))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
