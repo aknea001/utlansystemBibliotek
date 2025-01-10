8ff59b72adab0fee0d1856f9c8c032de4503c9d9
Aksel <aknea001@osloskolen.no>
2025-01-10 12:54:12 +0100

    la til bilde av cover på siden

--- bookInfoWeb/main.py
+++ bookInfoWeb/main.py
@@ -8,6 +8,28 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = getenv("FLASKPASSWD")
 
+def generateCover(navn, forfatter):
+    from PIL import Image, ImageDraw, ImageFont
+
+    image = Image.open("bookInfoWeb/static/baseCover.jpg")
+    draw = ImageDraw.Draw(image)
+
+    fontNavn = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 50)
+    fontForf = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 36)
+    textColor = (0, 0, 0)
+
+    navnTextLength = draw.textlength(navn, fontNavn)
+    xNavn = (image.width - navnTextLength) / 2
+    yNavn = image.height / 5
+
+    forfTextLength = draw.textlength(forfatter, fontForf)
+    xForf = (image.width - forfTextLength) / 2
+    yForf = image.height / 3
+
+    draw.text((xNavn, yNavn), navn, font=fontNavn, fill=textColor)
+    draw.text((xForf, yForf), forfatter, font=fontForf, fill=textColor)
+    image.save("bookInfoWeb/static/cover.jpg")
+
 @app.route("/<bokID>", methods=["GET", "POST"])
 def index(bokID):
     url = f"http://localhost:8000/bok/{bokID}"
@@ -24,6 +46,8 @@ def index(bokID):
             forfatter = bokInfo["forfatter"]
             sjanger = bokInfo["sjanger"]
             hylle = bokInfo["hylle"]
+
+            generateCover(navn, forfatter)
 
             elevNavn = f"{elevInfo['navn']['first']} {elevInfo['navn']['last']}"
             programfag = elevInfo["programfag"]
@@ -49,9 +73,9 @@ def index(bokID):
             return redirect(url_for("index", bokID=bokID))
         else:
             #print("returnerer")
-            
+
             response = requests.post(url, json={"return": True})
             return redirect(url_for("index", bokID=bokID))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, host="0.0.0.0")
