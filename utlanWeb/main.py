f7a1a6a6b55f50efa40a7f62a750ebe0761ceca6
Aksel <aknea001@osloskolen.no>
2025-01-15 11:16:06 +0100

    hvis du går til /profile uten å være logget inn går du til /login og blir redirecta til /profile etter du logger inn

--- utlanWeb/main.py
+++ utlanWeb/main.py
@@ -13,66 +13,6 @@ app.jinja_env.filters["zip"] = zip
 def index():
     return render_template("index.html")
 
-def generateCover(navn, forfattere, amount):
-    from PIL import Image, ImageDraw, ImageFont
-
-    for i in range(amount):
-        image = Image.open("bookInfoWeb/static/baseCover.jpg")
-        draw = ImageDraw.Draw(image)
-
-        fontNavn = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 50)
-        fontForf = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 36)
-        textColor = (0, 0, 0)
-
-        navnTextLength = draw.textlength(navn[i], fontNavn)
-        xNavn = (image.width - navnTextLength) / 2
-        yNavn = image.height / 5
-
-        forfTextLength = draw.textlength(forfattere[i], fontForf)
-        xForf = (image.width - forfTextLength) / 2
-        yForf = image.height / 3
-
-        draw.text((xNavn, yNavn), navn[i], font=fontNavn, fill=textColor)
-        draw.text((xForf, yForf), forfattere[i], font=fontForf, fill=textColor)
-        image.save(f"utlanWeb/static/cover{i}.jpg")
-
-@app.route("/profile")
-def elevInfo():
-    if "elevID" not in session:
-        return redirect(url_for("login"))
-
-    url = "http://localhost:8000/elev"
-
-    response = requests.get(url, headers={"elevID": str(session["elevID"])})
-
-    if response.status_code != 200:
-        return f"error connecting to server: {response.status_code}"
-    
-    data = response.json()
-    userInfo = data["userInfo"]
-
-    navn = userInfo["name"]
-
-    utlanLst = []
-    
-    if data["leid"]:
-        utlanInfo = data["utlanInfo"]
-        bokNavn = utlanInfo["bokNavn"]
-        bokForfattere = utlanInfo["bokForfattere"]
-
-        tempUtlanLst = []
-
-        for i in range(len(bokNavn)):
-            tempUtlanLst.append(bokNavn[i])
-            tempUtlanLst.append(bokForfattere[i])
-
-            utlanLst.append(tempUtlanLst)
-            tempUtlanLst = []
-
-    generateCover(bokNavn, bokForfattere, len(bokNavn))
-
-    return render_template("elevPage.html", navn=navn["first"], leid=data["leid"], utlan=utlanLst)
-
 def hash(passwd, salt):
     import hashlib
 
@@ -111,12 +51,77 @@ def login():
         return f"error: {response.status_code}"
     
     session["elevID"] = elevID
+
+    if "redirectUrl" in session:
+        return redirect(session["redirectUrl"])
+
     return redirect(url_for("index"))
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("index"))
+
+def generateCover(navn, forfattere, amount):
+    from PIL import Image, ImageDraw, ImageFont
+
+    for i in range(amount):
+        image = Image.open("bookInfoWeb/static/baseCover.jpg")
+        draw = ImageDraw.Draw(image)
+
+        fontNavn = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 50)
+        fontForf = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 36)
+        textColor = (0, 0, 0)
+
+        navnTextLength = draw.textlength(navn[i], fontNavn)
+        xNavn = (image.width - navnTextLength) / 2
+        yNavn = image.height / 5
+
+        forfTextLength = draw.textlength(forfattere[i], fontForf)
+        xForf = (image.width - forfTextLength) / 2
+        yForf = image.height / 3
+
+        draw.text((xNavn, yNavn), navn[i], font=fontNavn, fill=textColor)
+        draw.text((xForf, yForf), forfattere[i], font=fontForf, fill=textColor)
+        image.save(f"utlanWeb/static/cover{i}.jpg")
+
+@app.route("/profile")
+def elevInfo():
+    if "elevID" not in session:
+        session["redirectUrl"] = url_for("elevInfo")
+        return redirect(url_for("login"))
+
+    url = "http://localhost:8000/elev"
+
+    response = requests.get(url, headers={"elevID": str(session["elevID"])})
+
+    if response.status_code != 200:
+        return f"error connecting to server: {response.status_code}"
+    
+    data = response.json()
+    userInfo = data["userInfo"]
+
+    navn = userInfo["name"]
+
+    utlanLst = []
+    
+    if data["leid"]:
+        utlanInfo = data["utlanInfo"]
+        bokNavn = utlanInfo["bokNavn"]
+        bokForfattere = utlanInfo["bokForfattere"]
+
+        tempUtlanLst = []
+
+        for i in range(len(bokNavn)):
+            tempUtlanLst.append(bokNavn[i])
+            tempUtlanLst.append(bokForfattere[i])
+
+            utlanLst.append(tempUtlanLst)
+            tempUtlanLst = []
+
+    generateCover(bokNavn, bokForfattere, len(bokNavn))
+
+    return render_template("elevPage.html", navn=navn["first"], leid=data["leid"], utlan=utlanLst)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
