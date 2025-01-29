f79c3076193a3a5477219b8f2cb583ac9b1ea5a5
aknea001 <149372230+aknea001@users.noreply.github.com>
2025-01-29 14:09:42 +0100

    fjerner notifien etter bibliotekaren har funnet boka (eller ikke)

--- bookInfoWeb/static/updRes.js
+++ bookInfoWeb/static/updRes.js
@@ -13,6 +13,19 @@ websocket.addEventListener("message", ({ data }) => {
     const notifs = document.getElementById("notifs")
     const li = document.createElement("li")
 
-    li.innerHTML = `<b>${WSdata.tittel}</b> av ${WSdata.forfatter}, ${WSdata.hylle} - ${WSdata.elevID}`
+    li.innerHTML = `<b>${WSdata.tittel}</b> av ${WSdata.forfatter}, ${WSdata.hylle} - ${WSdata.elevID}
+    <button onclick="accept(this)">✓</button>  <button onclick="decline(this)">✘</button>`
     notifs.appendChild(li)
 })
+
+function accept(element) {
+    //api post request 
+
+    (element.parentElement).remove()
+}
+
+function decline(element) {
+    //api post request 
+
+    (element.parentElement).remove()
+}
